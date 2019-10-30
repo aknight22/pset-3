fa68b86eb43df71c3e682667ef72a9c3989f0b9b
Angelia Knight <aknight@ucvts.org>
2019-10-30 13:23:10 -0400

    exercise 1

--- src/change.js
+++ src/change.js
@@ -8,7 +8,7 @@ const DIME = 0.10;
 const NICKEL = 0.05;
 const PENNY = 0.01;
 
-const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
+let amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
 if (Number.isNaN(amount)) {
   console.log("Invalid.");
@@ -17,6 +17,13 @@ if (Number.isNaN(amount)) {
   console.log("Invalid.");
 
 } else {
-  if amount >= 0.25 = QUARTER
+  let quarters = (amount / QUARTER) - (amount % QUARTER);
+  let amountAfterQuarters = amount % QUARTER;
+  let dimes = amountAfterQuarters / DIME;
+  let amountAfterDimes = amountAfterQuarters % dimes;
+  let nickels = amountAfterDimes / NICKEL;
+  let amountAfterNickels = amountAfterDimes % nickels;
+  let pennies = amountAfterDimes / PENNY;
     // write your code for making change here
+    console.log(""+ quarters + " quarters, " + dimes + " dimes, " + nickels + " nickels,and " + pennies + " pennies.")
 }
